4e92fd038051494e553b73a66f19c3a739807aa1
kai <18336904168@163.com>
2022-04-07 09:17:17 +0800

    Initial commit

---
+++ src/main/java/com/mongo/mongo/config/MongoConfig.java
@@ -0,0 +1,21 @@
+package com.mongo.mongo.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoConfig {
+
+    public @Bean
+    MongoClient mongoClient() {
+        return MongoClients.create("mongodb://111.229.189.98:4000");
+    }
+
+    public @Bean
+    MongoTemplate mongoTemplate() {
+        return new MongoTemplate(mongoClient(), "productdb");
+    }
+}
