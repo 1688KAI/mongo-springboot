b58efa953ce23f3f8795eefc9efe09c208c800d2
kai <18336904168@163.com>
2022-04-08 08:57:05 +0800

    新建文件

--- src/test/java/com/mongo/mongo/MongoSpringbootApplicationTests.java
+++ src/test/java/com/mongo/mongo/MongoSpringbootApplicationTests.java
@@ -1,13 +1,51 @@
 package com.mongo.mongo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import java.util.List;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+import static org.springframework.data.mongodb.core.query.Query.query;
+import static org.springframework.data.mongodb.core.query.Update.update;
 
 @SpringBootTest
+@SelfLog
 class MongoSpringbootApplicationTests {
 
+
+    @Autowired
+    private MongoTemplate mongoOps;
+
     @Test
-    void contextLoads() {
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+
+        Person p = new Person("Joe", 34);
+
+        // 插入文档
+        mongoOps.insert(p);
+        log.info("Insert: " + p);
+
+        // 查询文档
+        p = mongoOps.findById(p.getId(), Person.class);
+        log.info("Found: " + p);
+
+        // 更新文档
+        mongoOps.updateFirst(query(where("name").is("Joe")), update("age", 35), Person.class);
+        p = mongoOps.findOne(query(where("name").is("Joe")), Person.class);
+        log.info("Updated: " + p);
+
+        // 删除文档
+        mongoOps.remove(p);
+
+        // Check that deletion worked
+        List<Person> people = mongoOps.findAll(Person.class);
+        log.info("Number of people = : " + people.size());
+        mongoOps.dropCollection(Person.class);
     }
+
 
 }
